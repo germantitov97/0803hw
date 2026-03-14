5280313b2960c6261094c7ff0bba09a9fb983bd6
germantitov97 <germangood2@gmail.com>
2026-03-14 20:45:17 +0200

    up

--- _targil.py
+++ _targil.py
@@ -7,7 +7,7 @@ def get_lucky_numbers(amount: int) -> tuple[int]:
 def input_until_lucky(lucky_numbers: tuple) -> int:
     attempts = 0
     _value_of_lucky_numbers = get_lucky_numbers(lucky_numbers)
-    print(_value_of_lucky_numbers)
+    print(_value_of_lucky_numbers) # only for checking purposes
     while True:
         user_input = int(input("put a number: "))
         attempts += 1
